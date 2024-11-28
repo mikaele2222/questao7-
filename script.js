30ef2af2d950cbade9c9f31edd8e9f9065b7d8a8
mikaele2222 <mfs52@aluno.ifal.edu.br>
2024-11-28 17:36:41 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+let tempC = parseFloat(prompt('Digite a sua temperatura em °C: '));
+
+let conversao = (temperatura C * 9/5) + 32;
+let tempF = conversao;
+
+alert('A sua temperatura de ' + temperatura C + '°C' + ' para Fahrenheit é de: ' + tempeeratur F + 'F');
+
+
+
+
+
